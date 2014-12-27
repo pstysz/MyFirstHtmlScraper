709c2426da7e34d9ca8c268abe40ddf41a03e752
pstysz@gmail.com <23PaweL74_gb>
2014-12-27 21:23:49 +0100

    First attempt to implement Django ORM

---
+++ MyFirstHtmlScraper/orm/manage.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+"""
+Command-line utility for administrative tasks.
+"""
+
+import os
+import sys
+import orm
+
+if __name__ == "__main__":
+    os.environ.setdefault(
+        "DJANGO_SETTINGS_MODULE",
+        "orm.settings"
+    )
+
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
