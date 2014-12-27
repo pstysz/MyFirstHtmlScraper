8b456976473b4920d6a6dbaa6b5e650cc36ba290
pstysz@gmail.com <23PaweL74_gb>
2014-12-27 22:36:43 +0100

    Creating models

--- MyFirstHtmlScraper/app/models.py
+++ MyFirstHtmlScraper/app/models.py
@@ -1,4 +1,14 @@
 from django.db import models
 
-class Test(models.Model):
-    field = models.CharField(max_length=255)
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+class Post(models.Model):
+    id = models.PositiveIntegerField(primary_key=True) #id will be get from post id so autoincrementation is undesirable
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=500)
+    url = models.URLField()
+    popularity = models.IntegerField() # count of 'wykopy'
+    image_url = models.URLField()
+    date = models.DateField()
+    category = models.ManyToManyField(Category)
