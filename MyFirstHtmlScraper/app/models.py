9a61d382d6317f0b9d5dd57b8d888d961b897029
pstysz@gmail.com <23PaweL74_gb>
2014-12-31 18:50:45 +0100

    Add popularity to Category model

--- MyFirstHtmlScraper/app/models.py
+++ MyFirstHtmlScraper/app/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=50, unique=True)
+    popularity = models.IntegerField(default=0) # how often category appears in posts
 
     def __str__(self):
         return self.name
