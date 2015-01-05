c565f100089064ec48adf5ae52479bc22505beb1
pstysz@gmail.com <23PaweL74_gb>
2015-01-05 20:32:10 +0100

    First attempt to scrap with multiprocessing (without shared variable/array yet)

--- MyFirstHtmlScraper/scrapers/pclab.py
+++ MyFirstHtmlScraper/scrapers/pclab.py
@@ -13,53 +13,44 @@ def start_scraping():
     base_soup = get_soup_for_url(first_site)
     no_of_sites = get_last_site_number(base_soup)
     url_list = get_urls_from_pattern(no_of_sites, PCLAB_SITE_PATTERN)
-
-    # debug
-    existing_ids = SourceArticle.select(SourceArticle.source_id).where(SourceArticle.source_type == SOURCE_TYPE['pclab'])
-    test_id = 61250
-    test_val = test_id in existing_ids
-    for t in existing_ids:
-        print(t.source_id)
-
-    # wrong method
-    articles_urls =  get_art_urls_from_list(url_list)
     no_of_pools = multiprocessing.cpu_count() * 2
-    pool = multiprocessing.Pool(no_of_pools)
-    #pool.map_async(scrap_site, urls)
-    #pool.close()
-    #pool.join()
+
+    pool = multiprocessing.Pool(no_of_pools)  
+    articles_urls_list = pool.map(get_art_urls_from_news_list, url_list)
+    pool.close()
+    pool.join()
+
+    #pool = multiprocessing.Pool(no_of_pools)
+    pool.map_async(create_article_from_url, articles_urls_list)
+    pool.close()
+    pool.join()
 
 def get_last_site_number(soup):
+    logging.debug('Pclab gets last site number')
     try:
-        #gets last site number from footer
-        logging.info('Getting last site number')
         page_number = soup.select('div.pages div.offset a')[-2].get_text()
         return int(page_number)
     except:  
         #TODO: Implement some nice exception handling
         raise 
 
-def get_art_urls_from_news_list(news_url, existing_ids):
+def get_art_urls_from_news_list(news_list_url):
     '''Returns urls to articles from passed url to list of articles.
-       Result is list of urls to new articles (not added to db yet)'''
+       Result is list of urls to new, unique articles (not added to db yet)'''
+    logging.debug('Pclab gets arts urls from passed news list')
     articles_urls = []
-
-    for url in url_list:
-        soup = get_soup_for_url(url)
-        ids = [int(re.findall(r'\d+', a.attrs.get('href'))[0]) for a in soup.select('div.list div.element div.title a')]
-        for id in ids:
-            try:
-                if SourceArticle.select().where((SourceArticle.source == SOURCE_TYPE['pclab']) & (SourceArticle.source_id == id)).count():
-                    continue
-            except:
-                pass
-        debug = existing_ids
-        #TODO: Filter urls by existing ids, add only new
+    soup = get_soup_for_url(news_list_url)
+    ids = [int(re.findall(r'\d+', a.attrs.get('href'))[0]) for a in soup.select('div.list div.element div.title a')]
+    for id in ids:
+        if SourceArticle.select().where((SourceArticle.source_type == SOURCE_TYPE['pclab']) & (SourceArticle.source_id == id)).exists():
+            return []  # if first article already exists, the rest is also already on db
+        articles_urls.append(PCLAB_ARTICLE_PATTERN.replace('{{article_id}}', str(id)))
     return articles_urls
 
 def create_article_from_url(article_url):
     '''Save article from passed url to database if not already exists
        Returns True for success and False if article already exists'''
+    logging.info('Pclab creates article from {0}'.format(article_url))
     article_id = int(re.findall(r'\d+', article_url)[0])
     if not SourceArticle.select().where((SourceArticle.source_type == SOURCE_TYPE['pclab']) & (SourceArticle.source_id == article_id)).exists():
         soup = get_soup_for_url(article_url)
