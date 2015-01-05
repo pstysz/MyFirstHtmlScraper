bbab7f0356ded30cf2a368cd6567432ecfd42c23
pstysz@gmail.com <23PaweL74_gb>
2015-01-05 17:50:23 +0100

    Revert "Group settings in regions"
    This reverts commit 63fe19670a34026b713168ab896e03ecbad9edc8.

--- MyFirstHtmlScraper/configuration/settings.py
+++ MyFirstHtmlScraper/configuration/settings.py
@@ -6,12 +6,10 @@ from peewee import *
 
 '''DO NOT INCLUDE THIS FILE TO SOURCE CONTROL!!!'''
 
-#region General Settings
 APP_DIR = os.path.dirname(os.path.dirname(__file__))
-logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
-#endregion General Settings
 
-#region Database Settings
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
+
 DBSETTINGS = {
     'mysql': {
         'database': 'content_generators',
@@ -24,19 +22,11 @@ DBSETTINGS = {
         'database': 'test.db'
     }
 }
-DB_HANDLER = MySQLDatabase(**DBSETTINGS['mysql'])
-#endregion Database Settings
 
-#region Kicker Settings
 KICKER_ROOT_URL = 'http://www.wykop.pl/wykopalisko/'
 KICKER_SUBSITE_PATTERN = KICKER_ROOT_URL + 'strona/{{site_number}}/' #{{site_number}} is taken from last site number
-#endregion Kicker Settings
 
-#region PcLab Settings
 PCLAB_SITE_PATTERN =  'http://pclab.pl/news-{{site_number}}-100.html' 
 PCLAB_ARTICLE_PATTERN = 'http://pclab.pl/news{{article_id}}.html'
-#endregion PcLab Settings
 
-#region Content Settings
-MAX_USAGE_CONT = 5  # how many times can single content be used to generate new article
-#endregion Content Settings
+DB_HANDLER = MySQLDatabase(**DBSETTINGS['mysql'])
