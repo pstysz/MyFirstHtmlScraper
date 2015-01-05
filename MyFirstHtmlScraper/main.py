6599530928cefd55b12947180954b22a163897b6
pstysz@gmail.com <23PaweL74_gb>
2015-01-05 20:33:12 +0100

    uncomment scraping part

--- MyFirstHtmlScraper/main.py
+++ MyFirstHtmlScraper/main.py
@@ -11,10 +11,6 @@ from tests.test import speed_test
 if __name__ == '__main__':
     initiate_db()
     start_time = time.time()
-    #digger.start_scraping()
-    #pclab.start_scraping() 
-    test_art = 'http://pclab.pl/news61250.html'
-    #pclab.create_article_from_url(test_art)
-    helpers.get_content_from_article(1, 61250)
-
-    logging.info('Scraping took {0}'.format(time.strftime("%H:%M:%S", time.gmtime(time.time() - start_time))))
+    digger.start_scraping()
+    pclab.start_scraping() 
+    logging.info('Finish, scraping took {0}'.format(time.strftime("%H:%M:%S", time.gmtime(time.time() - start_time))))
